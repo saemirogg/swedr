230e80624b244bb6dcbf6a472dd3a77757d90067
Sæmundur Rögnvaldsson <s.rognvaldsson@gmail.com>
2017-07-11 17:35:23 +0000

    Added exclude functionality. This excludes those with the particular diagnosis outsied the provided timeframe

--- R/add_event.R
+++ R/add_event.R
@@ -11,6 +11,7 @@ add_event <- function(dx,
                       before_time=c(-Inf,0),
                       after_time=c(0,Inf),
                       cancer=F,
+                      exclude_outside=F,
                       ignore=F){
 
   #If the search is in the cancer registry(cancer==T)
@@ -45,22 +46,34 @@ add_event <- function(dx,
   dia_data$time_from_incl <- days_diff(dia_data$incl_date,dia_data$INDATUMA)
 
 
+  #if exclusion is chosen as T then we will find those that have the diagnoses outside the timelimits provided and exclude them.
+  #We start by marking those that this applies to
+  if(exclude_outside==T){
+    exclude_dia_data <- filter(dia_data,(time_from_incl < before_time[1] | time_from_incl > before_time[2]) & (time_from_incl < after_time[1] | time_from_incl > after_time[2]))
+    part_data$exclude <- part_data$lop_nr %in% exclude_dia_data$lop_nr
+  }
+
   #if first, we will find the first diagnosis of dx within the dataset
   dia_data <- filter(dia_data,(time_from_incl > before_time[1] & time_from_incl < before_time[2]) | (time_from_incl > after_time[1] & time_from_incl < after_time[2]))  %>%
     group_by(lop_nr)
 
   if(relationship=="first"){
-    dia_data <- filter(dia_data,which.min(INDATUMA))
+    dia_data <- slice(dia_data,which.min(INDATUMA))
   }else if(relationship=="closest"){
-    dia_data <- filter(dia_data,which.min(abs(time_from_incl)))
+    dia_data <- slice(dia_data,which.min(abs(time_from_incl)))
   }else if(relationship=="last"){
-    dia_data <- filter(dia_data,which.max(INDATUMA))
+    dia_data <- slice(dia_data,which.max(INDATUMA))
   }else{
     stop("Relationship provided (",relationship,") is not recognized")
   }
   #Now creating a vector of the INDATUMA for those with diagnosis and NA if no diagnosis.
-  date_of_diagnosis <- dia_data$INDATUMA[match(part_data$lop_nr,dia_data$lop_nr)]
+  part_data$date_of_diagnosis <- dia_data$INDATUMA[match(part_data$lop_nr,dia_data$lop_nr)]
 
-  return(date_of_diagnosis)
+  #if excluding those with diagnoses outside the number 0 is put instead of NA or the date in the date of diagnosis
+  if(exclude_outside==T){
+    part_data$date_of_diagnosis <- ifelse(part_data$exclude,0,part_data$date_of_diagnosis)
+  }
+
+  return(part_data$date_of_diagnosis)
 
 }
