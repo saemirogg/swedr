82477e2ce37cb432a26d13b2b90dc95c8b4695dd
Solvi Rognvaldsson <sor16@hi.is>
2017-07-12 15:55:33 +0000

    extract_binary function added. Has yet to be reviewed

---
+++ R/extract_binary.R
@@ -0,0 +1,9 @@
+#' Transforms selected columns from the format provided by add_diagnoses
+#' to a binary representation, useful to do Cox regression
+extract_binary <-function(part_data,vars){
+  require(dplyr)
+  require(magrittr)
+  binary_data <- part_data %>% mutate_each_(funs=funs(is.na),vars=vars) %>%
+    mutate_each_(funs=funs(not),vars=vars)
+  return(binary_data)
+}
