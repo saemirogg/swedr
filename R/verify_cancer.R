66b4f4b0d120b1bbbaf9869ad673ceb43a5c5d29
Solvi Rognvaldsson <sor16@hi.is>
2017-07-12 15:44:04 +0000

    Changes made to verify cancer. A new argument added, remove, a logical argument indicating whether it should removed non verified cases and corresponding controls. If remove=F, the function adds a column to the output of getCC that states whether the cancer diagnosis of the corresponding case is also documented in the cancer registry

--- R/verify_cancer.R
+++ R/verify_cancer.R
@@ -7,44 +7,31 @@
 #'
 
 verify_cancer <- function(partdata,
-                          dx,wd=getwd()){
-  #load the data
+                          dx,wd=getwd(),remove=T){
+  #load the cancer registry
   setwd(wd)
   load("can.RData")
 
   dx <- paste(dx,collapse="|")
 
   ##Subsetting the data to only cancer diagnoses of interest
-  ##There is something wrong with this filter call,
-  ## The statement DIADAT > min(partdata$incl_date)+10000 does not make any sense
   can <- filter(can,
-                grepl(dx,ICD7) | grepl(dx,ICD9) | grepl(dx,ICDO10),
-                DIADAT > min(partdata$incl_date)+10000) #laggtime for the cancer registry. set here at 1 year check3
+                grepl(dx,ICD7) | grepl(dx,ICD9) | grepl(dx,ICDO10))
   #Adding verified column to data
-  #Sölvi: swapped the order of these two lines in order for cases_data to include verified as a col
-  partdata$verified <- partdata$lop_nr %in% can$LopNr
+  #A person is verified if its cancer diagnosis is recorded in the cancer registry or if its corresponding case
+  #is recorded in the cancer registry.
+  partdata <- partdata %>% mutate(verified =
+                          (partdata$lop_nr %in% can$LopNr & partdata$case) |
+                          partdata$lop_nr_case %in% can$LopNr)
 
-  #Taking out cases that are not recorded in the cancer registry
-  #Sölvi: changed partdata$lop_nr %in% can$LopNr to verified
-  cases_data <- filter(partdata,verified)
-
-  #Loading the control dataset to exclude controls not associated with verified cancer diagnoses
-  #Sölvi: I suggest not loading this dataset, but to extract the controls from data and reomve
-  #those that correspond to the cases not recorded from the cancer registry. In effect
-  #we need to change getCC s.t one more column is added, namely Lopnr_fall.
-  control_data <- data.frame()
-  if(any(!partdata$case)){
-    load("controls.RData")
-
-    #Creating subset with only these controls
-    control_group_data <- filter(controls,Lopnr_fall %in% cases_data$lop_nr)
-
-    control_data <- filter(partdata,lop_nr %in% control_group_data$Lopnr_kontroll)
-  }
   #Giving data on the verification process
-  warning(sum(partdata$case==T & partdata$verified==F),"(",round(sum(partdata$case==T & partdata$verified==F)/sum(partdata$case),digits = 2)*100,"%) cases are not in the cancer registry. They and their corresponding ",sum(!partdata$case)-nrow(control_data)," controls have been removed \n",
-          "There are ",sum(!(unique(can$LopNr) %in% partdata$lop_nr))," diagnoses in the cancer registry that are not in the cases register (laggtime included)\n",
-          "There are ",sum(partdata$case==F & partdata$verified==T)," controls that have a diagnosis of the given cancer")
-
-  return(rbind(cases_data,control_data))
+  warning(with(partdata,sum(case & !verified))," (",round(with(partdata,sum(case & !verified)/sum(partdata$case)),digits = 2)*100,
+          "%) cases are not in the cancer registry. There are ",sum(!partdata$case & partdata$lop_nr %in% can$LopNr),
+          " controls that have a diagnosis of the given cancer.")
+  if(remove){
+    partdata <- partdata %>% filter(verified) %>% select(-verified)
+    warning("These ",with(partdata,sum(case & !verified))," cases and their corresponding ",
+            with(partdata,sum(case & !verified))," controls have been removed.")
+  }
+  return(partdata)
 }
